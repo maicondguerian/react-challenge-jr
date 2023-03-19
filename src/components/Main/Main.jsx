cf7cdbfead7d27ace03db4331bb7e71b6af708d3
rincaveski <jrincaveski@gmail.com>
2023-03-19 18:34:29 -0300

    parte do main pronta

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import styles from './main.module.scss';
+import svg from './../../assets/pattern-divider-desktop.svg'
 
 export const Main = () => {
     return (
-        <main>
-            teste
+        <main className={styles.mainWrapper}>
+            <p><q>It is easy to sit up and take notice, what's difficult is getting up and taking action</q></p>
+            <div><img src={svg} alt=''/></div>
         </main>
     )
 }
