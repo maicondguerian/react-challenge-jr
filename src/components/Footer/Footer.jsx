a4314a3ac7d2666600bc19b17e98a992d09544b1
maicondguerian <maicondguerian@gmail.com>
2023-03-19 19:44:45 -0300

    hehe

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,12 @@
-import style from "./footer.module.scss";
+import styles from "./footer.module.scss";
 import svg from "./../../assets/icon-dice.svg";
 
 export const Footer = () => {
   return (
-    <footer className={style.footerContainer}>
-      <div className={style.buttonWrapper}>
-        <button>
+    <footer className={styles.footerContainer}>
+        <div className={styles.teste}>
           <img src={svg} alt="123" />
-        </button>
-      </div>
+        </div>
     </footer>
   );
 };
